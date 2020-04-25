3d053ec782103866b4d001c094fc1163253c053b
donquixote2u <donquixote2u@gmail.com>
2020-04-25 19:28:00 +1200

    modified:   init1.lua 	modified:   sentry.lua

--- sentry.lua
+++ sentry.lua
@@ -1,16 +1,19 @@
-function wifi_send()
+function send()
 DATA["field3"]=t.readNumber(6) -- read io12 again
 print("the temperature is  "..DATA["field3"])
-t = nil 
-ds18b20 = nil 
-package.loaded["ds18b20"]=nil
-sendData()                      -- send to Thingspeak
-initTimeout=5000       -- // timer in ms before restart
-initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() node.restart() end) 
+-- t = nil 
+-- ds18b20 = nil 
+-- package.loaded["ds18b20"]=nil
+-- sendData()                      -- send to Thingspeak
+initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() send() end) 
+Scrxpos=2 -- current position on screen - x coordinate
+Scrypos=2 -- current position on screen - y coordinate
+dprint(2,DATA["field3"].."°")
 end
 
 initTimeout=INTERVAL       -- // timer in ms
-initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(wifi_send) end) 
+-- initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(wifi_send) end) 
+initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,send) 
 t=require("ds18b20") 
-dofile("TSClient.lua")
+-- dofile("TSClient.lua")
 DATA["field3"]=t.readNumber(6) -- read io12 but first always rubbish
