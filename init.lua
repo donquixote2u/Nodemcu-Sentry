aabd5133d8474b11a003c63d9e0c8050e49c7612
donquixote2u <donquixote2u@gmail.com>
2021-07-15 14:59:15 +1200

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,23 @@
 -- temperature sensing via ESP8266 
 --manifest: ds, ds118b2, ide, WifiConnect, wificredentials
 -- Constants
-INTERVAL = 1200000      -- interval between data sends usec 
+INTERVAL = 120000      -- interval between data sends usec 
 wifiTrys     = 0      -- reset counter of trys to connect to wifi
 NUMWIFITRYS  = 20    -- Maximum number of WIFI Testings while waiting for connection
 function init_sentry()
+DATA={}
 dofile("sentry.lua")
+init_display()
+checkConnection(WifiOn)
+end
+function WifiOn() 
+WifiUp=true
 end
 initTimeout=5000       -- // timer in ms
 initTimer=tmr.create()  -- // start timer
 initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,init_sentry) 
 require("wificredentials")
 require("WifiConnect")
+WifiUp=false
+require("screen2")
+
